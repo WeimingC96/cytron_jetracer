032ce7e657979e84bc73bb73839727e2d7c0afbd
WeimingC96 <ramseychan410@gmail.com>
2021-07-14 15:17:49 +0200

    camera updated

--- scripts/camera.py
+++ scripts/camera.py
@@ -1,18 +1,12 @@
 #!/usr/bin/env python3
 
-#To view the output stream
-#Open web browser
-#http:/IP_ADDRESS:5000
-
-from flask import Flask, render_template, Response
 import cv2
+import numpy as np
 
 # gstreamer_pipeline returns a GStreamer pipeline for capturing from the CSI camera
 # Defaults to 1280x720 @ 60fps
 # Flip the image by setting the flip_method (most common values: 0 and 2)
 # display_width and display_height determine the size of the window on the screen
-
-app = Flask(__name__) #Flask web server
 
 # Gstreamer pipeline settings
 def gstreamer_pipeline(
@@ -47,14 +41,14 @@ def gstreamer_pipeline(
 def show_camera():
     # To flip the image, modify the flip_method parameter (0 and 2 are the most common)
     print("flip method: ", gstreamer_pipeline(flip_method=0))
-    #cap = cv2.VideoCapture(gstreamer_pipeline(flip_method=0), cv2.CAP_GSTREAMER)
-    cap = cv2.VideoCapture("v4l2src device=/dev/video0 ! videoconvert ! video/x-raw, format=BGR ! appsink", cv2.CAP_GSTREAMER)
+    cap = cv2.VideoCapture(gstreamer_pipeline(flip_method=0), cv2.CAP_GSTREAMER)
+    #cap = cv2.VideoCapture("v4l2src device=/dev/video0 ! videoconvert ! video/x-raw, format=BGR ! appsink", cv2.CAP_GSTREAMER)
     if cap.isOpened():
         window_handle = cv2.namedWindow("CSI Camera", cv2.WINDOW_AUTOSIZE)
         # Window
         while cv2.getWindowProperty("CSI Camera", 0) >= 0:
             ret_val, img = cap.read()
-            #cv2.imshow("CSI Camera", img)
+            cv2.imshow("CSI Camera", img)
             ret, buffer = cv2.imencode('.jpg', img)
             frame = buffer.tobytes()
             yield (b'--frame\r\n'
@@ -69,16 +63,5 @@ def show_camera():
     else:
         print("Unable to open camera")
 
-@app.route('/video_feed')
-def video_feed():
-    #Video streaming route. Put this in the src attribute of an img tag
-    return Response(show_camera(), mimetype='multipart/x-mixed-replace; boundary=frame')
-
-@app.route('/')
-def index():
-    """Video streaming home page."""
-    return render_template('index.html')
-
 if __name__ == "__main__":
     show_camera()
-    app.run(host= '192.168.1.87', debug=True, threaded=True) #Host ip address # was localhost
