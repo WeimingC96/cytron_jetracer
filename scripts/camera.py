6f8192720d11882c2ed54970b94beb64cf6a33c5
WeimingC96 <ramseychan410@gmail.com>
2021-07-14 18:35:14 +0200

    gstreamer class update

--- scripts/camera.py
+++ scripts/camera.py
@@ -18,16 +18,15 @@ class Camera:
         self.image_pub = rospy.Publisher("image_topic", Image, queue_size=1)
         self.bridge = CvBridge()
         self.display = False
+        self.capture_width=600,
+        self.capture_height=400,
+        self.display_width=600,
+        self.display_height=400,
+        self.framerate=30,
+        self.flip_method=0,
         
     # Gstreamer pipeline settings
-    def gstreamer_pipeline(
-        capture_width=600,
-        capture_height=400,
-        display_width=600,
-        display_height=400,
-        framerate=30,
-        flip_method=0,
-    ):
+    def gstreamer_pipeline(self):
         return (
             "nvarguscamerasrc ! "
             "video/x-raw(memory:NVMM), "
@@ -38,19 +37,19 @@ class Camera:
             "videoconvert ! "
             "video/x-raw, format=(string)BGR ! appsink"
             % (
-                capture_width,
-                capture_height,
-                framerate,
-                flip_method,
-                display_width,
-                display_height,
+                self.capture_width,
+                self.capture_height,
+                self.framerate,
+                self.flip_method,
+                self.display_width,
+                self.display_height,
             )
         )
 
     #Capture video frame
     def show_camera(self):
         # To flip the image, modify the flip_method parameter (0 and 2 are the most common)
-        cap = cv2.VideoCapture(self.gstreamer_pipeline(flip_method=0), cv2.CAP_GSTREAMER)
+        cap = cv2.VideoCapture(self.gstreamer_pipeline(), cv2.CAP_GSTREAMER)
         #cap = cv2.VideoCapture("v4l2src device=/dev/video0 ! videoconvert ! video/x-raw, format=BGR ! appsink", cv2.CAP_GSTREAMER)
         if self.display:
             if cap.isOpened():
